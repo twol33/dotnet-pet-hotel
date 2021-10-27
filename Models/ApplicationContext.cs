2818c269a5ee34338b0f77298ab52bcee462b1f0
Tou Xiong <tlxiong.prime@outlook.com>
2021-10-27 15:27:06 -0500

    created datatable for pets and petOwners

--- Models/ApplicationContext.cs
+++ Models/ApplicationContext.cs
@@ -22,6 +22,7 @@ namespace pet_hotel.Models
     public class ApplicationContext : DbContext
     {
         public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) {}
-        // public DbSet<MyClass> MyClassTable { get; set; }
+        public DbSet<Pet> Pets { get; set; }
+        public DbSet<PetOwner> PetOwners { get; set; }
     }
 }
