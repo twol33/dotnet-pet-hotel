74e439fe9856284fc44af58a66080c3755c3286e
Tou Xiong <tlxiong.prime@outlook.com>
2021-10-28 10:36:09 -0500

    updated the models to have appropriate properties, able to database update to have tables show in postico

---
+++ Migrations/20211027205218_CreatePetTable.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace dotnet_bakery.Migrations
+{
+    public partial class CreatePetTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PetOwners",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    emailAddress = table.Column<string>(type: "text", nullable: false),
+                    petCount = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PetOwners", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pets",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    breed = table.Column<int>(type: "integer", nullable: false),
+                    color = table.Column<int>(type: "integer", nullable: false),
+                    checkedInAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    petOwnerid = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pets", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PetOwners");
+
+            migrationBuilder.DropTable(
+                name: "Pets");
+        }
+    }
+}
--- Models/Pet.cs
+++ Models/Pet.cs
@@ -5,7 +5,38 @@ using System;
 
 namespace pet_hotel
 {
-    public enum PetBreedType {}
-    public enum PetColorType {}
-    public class Pet {}
+    public enum PetBreedType 
+    {
+        Shepherd, Poodle, Beagle, Bulldog, Terrier, Boxer, Labrador, Retriever
+    }
+    public enum PetColorType 
+    {
+        White, Black, Golden, Tricolor, Spotted
+    }
+    public class Pet 
+    {
+        public int id { get; set; }
+
+        [Required]
+        public string name { get; set; }
+
+        [Required]
+        public PetBreedType breed { get; set; }
+
+        [Required]
+        public PetColorType color { get; set; }
+
+        public DateTime checkedInAt { get; set; }
+
+        [Required][ForeignKey("PetOwners")]
+        public int petOwnerid { get; set; }
+
+        public PetOwner ownerPet { get; set; }
+
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public PetBreedType breedType {get; set;}
+
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public PetColorType colorType {get; set;}
+    } 
 }
